a704253a2c2fda998c87f20e0b9d17bc08549224
Domenic Beall <domenic.beall@aroflo.com>
2023-01-11 18:35:16 +1100

    Revert "Built and moved dist to root"
    This reverts commit 8942c6e9ca7123cf34779c3f29a55d8d7065a8b2.

--- esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Auth0CapacitorPlugin } from './definitions';
-declare const Auth0Capacitor: Auth0CapacitorPlugin;
-export * from './definitions';
-export { Auth0Capacitor };
--- esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const Auth0Capacitor = registerPlugin('Auth0Capacitor', {
-    web: () => import('./web').then(m => new m.Auth0CapacitorWeb()),
-});
-export * from './definitions';
-export { Auth0Capacitor };
-//# sourceMappingURL=index.js.map
